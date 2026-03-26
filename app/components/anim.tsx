42f7910d813e44b8da883f83118e697e3281de04
William-Johnny <wjguenon@icloud.com>
2026-03-26 13:19:50 +0100

    fix: removed bugged polaroid

--- app/components/anim.tsx
+++ app/components/anim.tsx
@@ -1,28 +1,41 @@
-"use client"
+"use client";
 
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import Polaroid from './polaroid';
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Polaroid from "./polaroid";
 
-gsap.registerPlugin(useGSAP, ScrollTrigger); // register the hook to avoid React version discrepancies 
+gsap.registerPlugin(useGSAP, ScrollTrigger); // register the hook to avoid React version discrepancies
 
 export const Anim = () => {
-	useGSAP(() => {
-		// gsap code here...
-		// gsap.to('.box', { x: 360, duration: 1.2, ease: 'expo.out', stagger: 0.1, scrollTrigger:{trigger: '.square-list', markers:true, start: 'top center', end: 'bottom center', scrub: true}}); // <-- automatically reverted
-		const tl = gsap.timeline()
-		tl.to('.box', { x: 360, duration: 1.2, ease: 'expo.out', stagger: 0.1, scrub: true}); // <-- automatically reverted
-		tl.to('.box', { y: -360, duration: 1.2, ease: 'expo.out', stagger: 0.1, scrub: true});
-
-	})
+  useGSAP(() => {
+    // gsap code here...
+    // gsap.to('.box', { x: 360, duration: 1.2, ease: 'expo.out', stagger: 0.1, scrollTrigger:{trigger: '.square-list', markers:true, start: 'top center', end: 'bottom center', scrub: true}}); // <-- automatically reverted
+    const tl = gsap.timeline();
+    tl.to(".box", {
+      x: 360,
+      duration: 1.2,
+      ease: "expo.out",
+      stagger: 0.1,
+      scrub: true,
+    }); // <-- automatically reverted
+    tl.to(".box", {
+      y: -360,
+      duration: 1.2,
+      ease: "expo.out",
+      stagger: 0.1,
+      scrub: true,
+    });
+  });
 
   return (
-	<>
-		<div style={{
-					height: '360px',
-				}}></div>
-		<Polaroid src="/reference.jpg" text="My polaroid" imgAlt="Polaroid"/>
-	</>
+    <>
+      <div
+        style={{
+          height: "360px",
+        }}
+      ></div>
+      {/* <Polaroid src="/reference.jpg" text="My polaroid" imgAlt="Polaroid"/> */}
+    </>
   );
-}
+};
